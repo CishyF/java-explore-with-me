bbeecfe585081ab6154c3b5f755598ca1439f7da
Cishy <fedorovnikita04@yandex.ru>
2024-04-01 10:13:59 +0300

    Создан контроллер и обработчик ошибок для работы со статистическими сущностями

---
+++ ewm-stats-service/stats-service/src/main/java/ru/practicum/exception/ErrorResponse.java
@@ -0,0 +1,20 @@
+package ru.practicum.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Value;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+import static ru.practicum.util.Constants.DATE_TIME_PATTERN;
+
+@Value
+@Builder
+public class ErrorResponse {
+
+    String message;
+    HttpStatus status;
+    @JsonFormat(pattern = DATE_TIME_PATTERN)
+    LocalDateTime timestamp;
+}
